724446b8d104dc1363ba9c6b4065f1d8cfb1bf6c
jorgtron <jorgtron@gmail.com>
2014-04-26 16:09:50 +0200

    fixed wrong daily value change

--- assets/javascripts/alert.js
+++ assets/javascripts/alert.js
@@ -1,7 +1,5 @@
 (function() {
 
-
-
   // start
   setTimeout(function(){
 
@@ -72,7 +70,7 @@
     }.bind(this));
   */  
 
-  if($('body').hasClass('logged_in')) {    
+  if(!$('div.not-logged-in-avatar').length) {    // if not logged in
 
     Discourse.ajax("/stock/stock_data", {
       type: "GET",
@@ -172,19 +170,37 @@
 
 //        console.log('animating');
         $('.stock_change_percent strong').numberAnimate('set', percent_change);
-        $('.stock_my_total_value strong').numberAnimate('set', stock_my_total_value);
-        $('.value_change strong').numberAnimate('set', value_change.toFixed(0));
-        $('.value_change_percent strong').numberAnimate('set', value_change_percent.toFixed(1));
+        if(stock_my_total_value != undefined) { $('.stock_my_total_value strong').numberAnimate('set', stock_my_total_value); }
+        if(value_change != undefined) { $('.value_change strong').numberAnimate('set', value_change.toFixed(0)); }
+        if(!isNaN(value_change_percent)) { $('.value_change_percent strong').numberAnimate('set', value_change_percent.toFixed(1)); }
 
         
+        //stock.percent_change = '-90%';
         
+        // if negative:
 
-        if(percent_change .indexOf("-")){
-          var value_change_today = stock_my_total_value / (stock.percent_change.replace("%","").replace("+","") - 100) * -1;
+        //percent_change = '-19.5%';
+        if(percent_change.indexOf("-")){
+          
+          value_yesterday = stock_my_total_value * 100;
+          value_yesterday = value_yesterday / (100 - (stock.percent_change.replace("%","").replace("+","")) * -1);
+          console.log(value_yesterday);
+          console.log('diff:' + (stock_my_total_value - value_yesterday));
+
+          var value_change_today = stock_my_total_value - value_yesterday;
+          console.log(value_change_today);
+
         } else {
-          var value_change_today = stock_my_total_value / (stock.percent_change.replace("%","").replace("+","") - 100);
-        }
-        //console.log(value_change_today);
+          
+          value_yesterday = stock_my_total_value * 100;
+          value_yesterday = value_yesterday / (100 - (stock.percent_change.replace("%","").replace("+","")) * -1);
+          console.log(value_yesterday);
+          console.log('diff:' + (stock_my_total_value - value_yesterday));
+
+          var value_change_today = stock_my_total_value - value_yesterday;
+          console.log(value_change_today);
+      }
+        
 
         $('.value_change_today strong').numberAnimate('set', value_change_today.toFixed(0));
 
